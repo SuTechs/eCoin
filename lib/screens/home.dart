57ad45c4e99bae99d95163ca1076d5e4d7230c92
SuTechs <sumit123210@gmail.com>
2020-07-05 19:14:26 +0530

    added candle chart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,8 +4,16 @@ import 'package:ecoin/widgets/dash.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ecoin/constants.dart';
+import 'package:flutter_candlesticks/flutter_candlesticks.dart';
 
 class Home extends StatelessWidget {
+  final List sampleData = [
+    {"open": 50.0, "high": 100.0, "low": 40.0, "close": 80, "volumeto": 5000.0},
+    {"open": 80.0, "high": 90.0, "low": 55.0, "close": 65, "volumeto": 4000.0},
+    {"open": 65.0, "high": 120.0, "low": 60.0, "close": 90, "volumeto": 7000.0},
+    {"open": 90.0, "high": 95.0, "low": 85.0, "close": 80, "volumeto": 2000.0},
+    {"open": 80.0, "high": 85.0, "low": 40.0, "close": 50, "volumeto": 3000.0},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,11 +120,18 @@ class Home extends StatelessWidget {
                       ),
 
                       Container(
-                        color: Colors.red,
+                        //color: Colors.red,
                         height: 330,
+                        child: OHLCVGraph(
+                            data: sampleData,
+                            enableGridLines: true,
+                            volumeProp: 0,
+                            gridLineAmount: 5,
+                            gridLineColor: Colors.grey[300],
+                            gridLineLabelColor: Colors.grey),
                       ),
 
-                      SizedBox(height: 10),
+                      SizedBox(height: 20),
 
                       /// Chart Buttons
                       Row(
--- lib/screens/home_details.dart
+++ lib/screens/home_details.dart
@@ -2,9 +2,18 @@ import 'package:ecoin/screens/login.dart';
 import 'package:ecoin/widgets/customContainer.dart';
 import 'package:ecoin/widgets/dash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_candlesticks/flutter_candlesticks.dart';
 import '../constants.dart';
 
 class HomeDetails extends StatelessWidget {
+  final List sampleData = [
+    {"open": 50.0, "high": 100.0, "low": 40.0, "close": 80, "volumeto": 5000.0},
+    {"open": 80.0, "high": 90.0, "low": 55.0, "close": 65, "volumeto": 4000.0},
+    {"open": 65.0, "high": 120.0, "low": 60.0, "close": 90, "volumeto": 7000.0},
+    {"open": 90.0, "high": 95.0, "low": 85.0, "close": 80, "volumeto": 2000.0},
+    {"open": 80.0, "high": 85.0, "low": 40.0, "close": 50, "volumeto": 3000.0},
+  ];
+
   static const route = 'HomeDetails';
 
   @override
@@ -105,11 +114,18 @@ class HomeDetails extends StatelessWidget {
                     ),
 
                     Container(
-                      color: Colors.red,
+                      //color: Colors.red,
                       height: 330,
+                      child: OHLCVGraph(
+                          data: sampleData,
+                          enableGridLines: true,
+                          volumeProp: 0,
+                          gridLineAmount: 5,
+                          gridLineColor: Colors.grey[300],
+                          gridLineLabelColor: Colors.grey),
                     ),
 
-                    SizedBox(height: 10),
+                    SizedBox(height: 20),
 
                     /// Chart Buttons
                     Row(
